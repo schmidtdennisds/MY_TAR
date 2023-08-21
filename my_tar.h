2cde99fe8f0c18a332a3eafeacd89539f7a1c23d
dennis schmidt <schmidt_d@qwasar.io>
2023-08-21 12:57:21 +0000

    Added Header file

---
+++ my_tar.h
@@ -0,0 +1,46 @@
+#ifndef MY_TAR
+#define MY_TAR
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string.h>
+#include <sys/stat.h> 
+#include <sys/types.h>
+#include <fcntl.h>
+
+
+typedef struct s_options {
+    bool c;
+    bool r;
+    bool f;
+    bool t;
+    bool u;
+    bool x;
+} options;
+
+typedef struct s_posix_header
+{ 
+  char name[100];
+  char size[12];
+  char mtime[12];
+} posix_header;
+
+typedef struct s_arguments {
+    char* name;
+    struct s_arguments* next;
+} arguments;
+
+typedef struct s_data {
+    options* options;
+    arguments* arguments;
+} data;
+
+#define SIZE_OF_NAME 100
+#define SIZE_OF_FILESIZE 12
+#define SIZE_OF_TIME 12
+#define BLOCKSIZE 512
+#define ENDBLOCKSIZE 1024
+
+#endif
